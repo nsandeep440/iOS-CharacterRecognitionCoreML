1d4718d994da7136a0bcc7f76c2a82c537aa51ef
Sandeep Nakka <nsandeep440@gmail.com>
2019-05-05 17:54:48 +0530

    Created demo project

---
+++ Demo/CharacterRecognitionDemo/CharacterRecognitionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CharacterRecognitionDemo
+//
+//  Created by Sandeep N on 05/05/19.
+//  Copyright © 2019 fishTheData. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
